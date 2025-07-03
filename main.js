072c709b15efbe4cf1bd2797d69a7f9c4f6135f6
AlexSch95 <95.schulz@googlemail.com>
2025-07-03 09:37:35 +0200

    Added addTodo() Function to add a Todo to the Array todosList

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("JavaScript Datei geladen")
+
+let todosList = [];
+
+function addTodo() {
+    // Erster Schritt: Eingabe aus dem Input Field rausholen (mit überprüfung ob eine Eingabe existiert)
+    const inputField = document.getElementById("todoInput");
+    const inputFieldValue = inputField.value;
+    // Überprüfung ob Eingabe Leer ist
+    if (inputFieldValue === "") {
+        alert("Eingabefeld ist Leer, Bitte gib ein To-Do ein");
+        return;
+    }
+    // Zweiter Schritt: Eingabe in todosList hinzufügen
+    todosList.push(inputFieldValue); //wert aus dem Eingabefeld wird dem Array todosList hinzugefügt
+    // Dritter Schritt: Eingabe-Feld nach dem hinzufügen leeren
+    inputField.value = "" //Input Field wird geleert
+    console.log(`Das neue Todo ist: ${inputFieldValue}`)
+    console.log(`Alle To-Dos: `, todosList)
+    alert("To-Do erfolgreich hinzugefügt: " + inputFieldValue);
+}
