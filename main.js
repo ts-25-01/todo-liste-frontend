c5b141a8a9c8378fb1eb05ee782f8488fef461be
Helen Haveloh <h.havelohh@gmail.com>
2025-07-09 12:53:28 +0200

    added mockapi integration

--- main.js
+++ main.js
@@ -1,15 +1,130 @@
 console.log("JavaScript Datei geladen")
 
-let todosList = [
-    { "title": "waschen", "completed": true, "date": "02.07.2025" },
-    { "title": "putzen", "completed": false, "date": "03.07.2025" }
-];
+// Veränderung, dass wir ein leeres Array initialiseren, das wir gleich mit einer Funktion füllen, wo wir die API nach Daten fragen
+let todosList = [];
 
-function changeStatus(index) {
+// Hilfsfunktionen, um die Loading Spinner jeweils anzuzeigen bzw. wieder zu verstecken
+function showLoading() {
+    document.getElementById('loadingSpinner').style.display = 'block';
+}
+
+function hideLoading() {
+    document.getElementById('loadingSpinner').style.display = 'none';
+}
+
+document.getElementById('loadingSpinnerSwitchButton').addEventListener('click', () => {
+    if (document.getElementById('loadingSpinner').style.display === 'block') {
+        hideLoading();
+        let isLoading = false;
+        setButtonLoading(isLoading);
+    } else {
+        showLoading();
+        let isLoading = true;
+        setButtonLoading(isLoading);
+    }
+})
+
+function setButtonLoading(isLoading) {
+    const button = document.getElementById('addButton');
+    const text = document.getElementById('addButtonText');
+    const spinner = document.getElementById('addButtonSpinner');
+
+    if (isLoading) {
+        button.disabled = true;
+        text.textContent = "Wird hinzugefügt";
+        spinner.style.display = 'inline-block';
+    } else {
+        button.disabled = false;
+        text.textContent = "Hinzufügen";
+        spinner.style.display = 'none';
+    }
+}
+
+// Funktion, um die Fehlermeldung bei Bedarf 5 Sekunden anzuzeigen
+function showError(message) {
+    document.getElementById('errorText').textContent = message;
+    document.getElementById('errorMessage').style.display = 'block';
+    document.getElementById('errorMessage').classList.add('show');
+    setTimeout(() => {
+        document.getElementById('errorMessage').classList.remove('show');
+        setTimeout(() => {
+            document.getElementById('errorMessage').style.display = 'none';
+        }, 300)
+    }, 3000);
+}
+
+// Funktion, um sich die Todos von der API zu laden
+async function loadTodosFromAPI() {
+    // Logge dir aus, dass wir jetzt die Todos von der API laden
+    console.log("Lade Todos von der API...");
+    // Rufe Funktion showLoading auf, damit wir den Ladezustand angezeigt bekommen
+    showLoading();
+    try {
+        // fetche dir die Daten von der API
+        const response = await fetch('https://686e0a5fc9090c49538803f9.mockapi.io/api/todos');
+        // Transformiere das Response-Objekt in der Konstante response in ein json-Format
+        const todosFromAPI = await response.json();
+        // Schreibe diese todosFromAPI im json-Format in unsere Todos-Liste
+        todosList = todosFromAPI;
+        console.log("Todos geladen...", todosList);
+        // Wir haben jetzt unsere Todos von der API geholt
+        // Jetzt wollen wir diese Todos in der Liste darstellen
+    } catch (error) {
+        console.error("Fehler beim Laden", error);
+        showError("Todos konnten nicht geladen werden. Offline-Modus aktiviert");
+        // Gehe auf Fallback zurück
+        todosList = [
+            { "title": "waschen", "completed": true, "date": "02.07.2025" },
+            { "title": "putzen", "completed": false, "date": "03.07.2025" }
+        ];
+    } finally {
+        renderToDos();
+        hideLoading();
+    }
+
+
+
+
+}
+loadTodosFromAPI();
+
+
+async function changeStatus(index) {
     // Status switchen von completed im ToDos Objekt
-    todosList[index].completed = !todosList[index].completed;
-    // Aufruf der Render-ToDos für aktuelle Ansicht
+    // todosList[index].completed = !todosList[index].completed;
+    const todo = todosList[index];
+    const newStatus = !todo.completed;
+    todo.completed = newStatus;
     renderToDos();
+    try {
+        console.log("ändere Status für das Todo", todo);
+        console.log("todo id ", todo.id);
+        console.log("neuer Status", newStatus);
+        console.log("Ganzes Array", todosList);
+        // Fetch um Status des Todos zu aktualisieren
+        // const APIURL = `https://686e0a5fc9090c49538803f9.mockapi.io/api/todos/${todo.id}`
+        // console.log("Das ist die URL", APIURL);
+        const response = await fetch(`https://686e0a5fc9090c49538803f9.mockapi.io/api/todos/${todo.id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json', },
+            body: JSON.stringify({
+                title: todo.title,
+                completed: newStatus,
+                date: todo.date,
+                id: todo.id
+            })
+        })
+        if (!response.ok){
+            throw new Error(`HTTP Error: ${response.status}`)
+        }
+        console.log("Status geändert");
+        // renderToDos();
+    } catch (error) {
+        console.error("Fehler beim Status ändern", error);
+
+    }
+    // Aufruf der Render-ToDos für aktuelle Ansicht
+    // renderToDos();
 }
 
 function renderToDos() {
@@ -52,31 +167,52 @@ function renderToDos() {
 
 }
 
-function addTodo() {
+async function addTodo() {
     // Erster Schritt: Eingabe aus dem Input Field rausholen (mit überprüfung ob eine Eingabe existiert)
     const inputField = document.getElementById("todoInput");
     const inputFieldValue = inputField.value;
     //
     // Überprüfung ob Eingabe Leer ist
     if (inputFieldValue === "") {
-        // alert("Eingabefeld ist Leer, Bitte gib ein To-Do ein");
+        showError("Eingabe darf nicht leer sein");
         return;
     }
-    // Konstruiere das To-Do als Objekt - bsp: {"title": "waschen", "completed": true, "date": "02.07.2025"},
-    const newTodoObject = {
-        title: inputFieldValue,
-        completed: false,
-        date: "Platzhalter",
-    }
-    // Zweiter Schritt: Eingabe in todosList hinzufügen
-    todosList.push(newTodoObject); //wert aus dem Eingabefeld wird dem Array todosList hinzugefügt
-    // Dritter Schritt: Eingabe-Feld nach dem hinzufügen leeren
-    inputField.value = "" //Input Field wird geleert
-    renderToDos();
+    setButtonLoading(true);
+    try {
+        console.log("Erstelle neues Todo über API...");
+        // API-Call zum Hinzufügen des neuen Todos
+        // über eine POST-Request
+        const response = await fetch('https://686e0a5fc9090c49538803f9.mockapi.io/api/todos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: inputFieldValue,
+                completed: false
+            })
+        });
+        const newTodoFromAPI = await response.json();
+        console.log("Todo hinzugefügt", newTodoFromAPI);
+        // Eingabefeld wieder geleert
+        inputField.value = "" //Input Field wird geleert
 
-    console.log(`Das neue Todo ist: `, newTodoObject)
-    console.log(`Alle To-Dos: `, todosList)
-    // alert("To-Do erfolgreich hinzugefügt: " + inputFieldValue);
+        // Zweiter Schritt: Eingabe in todosList hinzufügen
+        todosList.push(newTodoFromAPI); //wert aus dem Eingabefeld wird dem Array todosList hinzugefügt
+        // loadTodosFromAPI();
+        // Dritter Schritt: Eingabe-Feld nach dem hinzufügen leeren
+        renderToDos();
+    } catch (error) {
+        console.error("Fehler beim Laden", error);
+        showError("Todo konnte nicht erstellt werden");
+    } finally {
+        setButtonLoading(false);
+    }
+
+
+    // console.log(`Das neue Todo ist: `, newTodoObject)
+    // console.log(`Alle To-Dos: `, todosList)
+    // // alert("To-Do erfolgreich hinzugefügt: " + inputFieldValue);
 }
 renderToDos();
 
@@ -117,7 +253,7 @@ function editToDo(index) {
 // Löschen-Button hat die ID: confirmDeleteButton
 // Sobald diese Aktion passiert, wird eine Funktion ausgeführt
 // Diese Funktion beinhaltet dann das Löschen des Todos aus der Liste
-document.getElementById('confirmDeleteButton').addEventListener('click', function () {
+document.getElementById('confirmDeleteButton').addEventListener('click', () => {
     if (deleteIndex !== null) {
         todosList.splice(deleteIndex, 1);
         deleteIndex = null;
@@ -127,3 +263,16 @@ document.getElementById('confirmDeleteButton').addEventListener('click', functio
         bootstrap.Modal.getInstance(modalElement).hide();
     }
 })
+
+// function buttonClicked() {
+//     if (deleteIndex !== null) {
+//         todosList.splice(deleteIndex, 1);
+//         deleteIndex = null;
+//         renderToDos();
+
+//         const modalElement = document.getElementById('deleteModal');
+//         bootstrap.Modal.getInstance(modalElement).hide();
+//     }
+// }
+
+// document.getElementById('confirmDeleteButton').addEventListener('click', buttonClicked())
